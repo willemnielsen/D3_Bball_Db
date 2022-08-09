1664edc2c4eaa275460633b4d71d50c69635dd8b
willemnielsen <wnielsen@vassar.edu>
2022-08-09 17:15:30 -0400

    Made flask pull directly from scrape data.

--- d3/models.py
+++ d3/models.py
@@ -1,12 +1,20 @@
+from sqlalchemy import PickleType
+
 from d3 import db
 
 
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    school = db.Column(db.String(100), unique=True, nullable=False)
-    conference = db.Column(db.String(100), nullable=False)
-    cityandstate = db.Column(db.String(100))
-    region = db.Column(db.String(100))
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    players = db.relationship('Player', backref='team', lazy=True)
 
     def __repr__(self):
         return f"School('{self.school}','{self.conference}')"
+
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    stats = db.Column(PickleType)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+
--- d3/routes.py
+++ d3/routes.py
@@ -1,13 +1,57 @@
 from flask import render_template, url_for, flash, redirect
 import pickle
-from d3 import app, d3schools, players
+import sys
+from d3 import app, d3schools, players, db
 from d3.forms import AddTeam
 from pretty_html_table import build_table
-from d3.models import Team
+from d3.models import Team, Player
+from flask_table import Table, Col, create_table
 import os
+from d3scrape import scrape
 
-with open('d3/tables/pandas_tables.pkl', 'rb') as file:
-    team_tables = pickle.load(file)
+
+# class RenameUnpickler(pickle.Unpickler):
+#     def find_class(self, module, name):
+#         renamed_module = module
+#         if module == "teamandpage":
+#             renamed_module = "d3scrape.teamandpage"
+#         return super(RenameUnpickler, self).find_class(renamed_module, name)
+#
+#
+# def renamed_load(file_obj):
+#     return RenameUnpickler(file_obj).load()
+#
+#
+# with open('d3/teams/teams_with_players.pkl', 'rb') as file:
+#     teams = renamed_load(file)
+# with open('d3/teams/teams_with_players.pkl', 'wb') as file:
+#     pickle.dump(teams, file)
+
+with open('d3/teams/teams_with_players.pkl', 'rb') as file:
+    teams = pickle.load(file)
+
+db.drop_all()
+db.create_all()
+dbteams = []
+for team in teams:
+    dbteam = Team(name=team.name)
+    dbplayers = []
+    try:
+        players = team.players
+    except AttributeError:
+        continue
+    for player in players:
+        dbplayers.append(Player(name=player.name, stats=player.stats, team_id=dbteam.id))
+    dbteam.players = dbplayers
+    db.session.add(dbteam)
+    db.session.commit()
+
+
+
+
+
+
+
 
 
 # class Team:
@@ -28,8 +72,8 @@ with open('d3/tables/pandas_tables.pkl', 'rb') as file:
 @app.route('/')
 @app.route('/home')
 def home():
-    links = team_tables
-    return render_template('home.html', links=links)
+    teams = Team.query.all()
+    return render_template('home.html', teams=teams)
 
 
 @app.route('/addteam', methods=['GET', 'POST'])
@@ -41,10 +85,21 @@ def addteam():
     return render_template('addteam.html', title='Add Team', form=form)
 
 
+
+
+
 @app.route('/<team_name>')
 def index(team_name):
-    html_tables = [build_table(team_tables[team_name], 'blue_light')]
-    return render_template('team.html', tables=html_tables)
+    PlayerTable = create_table('PlayerTable')
+    team = Team.query.filter_by(name=team_name).first()
+    for player in team.players:
+        player.stats['Name'] = player.name
+    first_player = team.players[0]
+    for stat in first_player.stats.keys():
+        PlayerTable.add_column(str(stat), Col(str(stat)))
+    player_stats = [player.stats for player in team.players]
+    table = PlayerTable(player_stats)
+    return render_template('team.html', table=table)
 
 
 
