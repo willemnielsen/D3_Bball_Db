adbb81bae669581c486941f16161ce87556d894e
willemnielsen <wnielsen@vassar.edu>
2022-08-10 15:59:59 -0400

    added bcrypt and login manager

--- d3/__init__.py
+++ d3/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '32528c7d0c362c0c0478df3ab12d8b6a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from d3 import routes
