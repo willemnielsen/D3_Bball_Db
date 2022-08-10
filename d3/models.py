daf78930f391e0a5d69c3a9c0d2f542bd5fcba9d
willemnielsen <wnielsen@vassar.edu>
2022-08-10 16:07:51 -0400

    Added User and Post models. Defined login_manager

--- d3/models.py
+++ d3/models.py
@@ -1,7 +1,12 @@
 from sqlalchemy import PickleType
+from datetime import datetime
+from flask_login import UserMixin
+from d3 import db, login_manager
 
-from d3 import db
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,7 +14,7 @@ class Team(db.Model):
     players = db.relationship('Player', backref='team', lazy=True)
 
     def __repr__(self):
-        return f"School('{self.school}','{self.conference}')"
+        return f"Team('{self.name}')"
 
 
 class Player(db.Model):
@@ -17,4 +22,26 @@ class Player(db.Model):
     name = db.Column(db.String(100), unique=False, nullable=False)
     stats = db.Column(PickleType)
     team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
