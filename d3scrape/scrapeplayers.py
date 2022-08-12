d9c02752cd520b47850739b0925b34d2e1780aec
willemnielsen <wnielsen@vassar.edu>
2022-08-12 11:39:53 -0400

    updated get_players from lineup

--- d3scrape/scrapeplayers.py
+++ d3scrape/scrapeplayers.py
@@ -2,6 +2,7 @@ from d3scrape.scrapeindpages import ScrapeIndPages
 from d3scrape.scrapetools import ScrapeTools as st
 from bs4 import BeautifulSoup as bs
 from d3scrape.teamandpage import Player
+from uuid import uuid4
 
 base = '/Users/erichegonzales/PycharmProjects/playerscrape/mp/'
 class ScrapePlayers:
@@ -76,35 +77,48 @@ class ScrapePlayers:
     @staticmethod
     def get_players_from_lineup():
         tables = st.load(base + 'lineup_tables.pkl')
-        teams = st.load(base + 'updated_teams.pkl')
+        teams = st.load(base + 'updt_plys.pkl')
         for team in teams:
             try:
                 table = bs(tables[team.name], 'html.parser')
             except KeyError:
                 continue
             else:
-                table_body = table.tbody
-                rows = table_body.find_all('tr')
+                if table.thead:
+                    head_row = table.thead.find('tr')
+                    body_rows = table.tbody.find_all('tr')
+                else:
+                    head_row = table.find('tr')
+                    body_rows = table.find_all('tr')[1:]
+                ths = head_row.find_all('th')
+                headers = [th.get_text() for th in ths]
                 players = []
-                for row in rows:
+                for row in body_rows:
                     stats = {}
-                    for idx, stat in enumerate(row.find_all('td')):
-                        if idx == 0:
-                            col = 'Number'
-                        elif idx == 1:
+                    for i, (stat, head) in enumerate(zip(row.find_all('td'), headers)):
+                        if i == 1:
                             try:
-                                name = stat.a.get_text().strip().replace(' ', '').replace('\n', ' ')
+                                new_val = stat.a.get_text().strip().replace(' ', '').replace('\n', ' ')
                                 bio = team.baseurl + stat.a.get('href')
-                                continue
                             except AttributeError:
-                                continue
+                                new_val = 'unknown'
+                                bio = 'unknown'
                         else:
-                            col = stat.get_text()
-                        new_val = stat.get_text()
-                        stats[col] = new_val
-                    players.append(Player(team.name, name, bio=bio, stats=stats))
+                            new_val = stat.get_text().strip().replace(' ', '').replace('\n', ' ')
+                        stats[head] = new_val
+                    players.append(Player(uuid4().time_low, team.name, bio=bio, stats=stats))
                 team.players = players
         st.dump(teams, base + 'updated_teams_with_players.pkl')
+
+
+    @staticmethod
+    def teams_with_players():
+        teams = st.load(base + 'updated_teams_with_players.pkl')
+        count = 0
+        for team in teams:
+            if team.players:
+                count += 1
+        print(count)
 
 
 
@@ -129,4 +143,4 @@ class ScrapePlayers:
     #     with open('../mp/pandas_tables.pkl', 'wb') as file:
     #         pickle.dump(pandas_tables, file)
 if __name__ == '__main__':
-    ScrapePlayers.get_players_from_lineup()
+    ScrapePlayers.teams_with_players()
